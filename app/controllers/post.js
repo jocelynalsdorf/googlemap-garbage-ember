995e22d1701c55d1d2f9873426360187b74324dd
jocelynalsdorf <jfrodsla@gmail.com>
2015-06-19 07:56:00 -0700

    edit and delete functions for single post

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,3 +1,26 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({});
+export default Ember.Controller.extend({
+  isEditing: false,
+ 
+  actions: {
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    save: function() {
+      var post = this.get('model');
+      post.set('title', this.get('model.title'));
+      post.set('body', this.get('model.body'));
+      post.save();
+      this.set('isEditing', false);
+    },
+    delete: function() {
+      var post = this.get('model');
+      // var users = post.get('users');
+     	post.destroyRecord();
+     	this.transitionToRoute('posts');
+      }
+		
+	
+	}
+});
