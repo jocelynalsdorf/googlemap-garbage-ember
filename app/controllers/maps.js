ed5945cb4494fa6a067c68d08181806c52d2f609
jocelynalsdorf <jfrodsla@gmail.com>
2015-06-24 07:53:14 -0700

    clean up

--- app/controllers/maps.js
+++ app/controllers/maps.js
@@ -7,17 +7,8 @@ export default Ember.Controller.extend({
     	var addStreet = this.get('addStreet');
     	var addCity = this.get('addCity');
     	var addState = this.get('addState');
-    	var map;
-      // var latitude = this.get(addresses);
-       // var longitude = this.get(address.geometry.viewport.northeast.lng);
 
-
-			//  map = new google.maps.Map(document.getElementById('map-canvas'), {
-			//     zoom: 15,
-			//     center: {lat:  36.8466744802915, lng:  -119.6881141697085}
-			// });
-
-			 // alert(latitude);
+      
       this.transitionToRoute('map', addNum, addStreet, addCity, addState);
 
 
@@ -26,5 +17,3 @@ export default Ember.Controller.extend({
 });
 
 
-
-//https://www.google.com/maps/embed/v1/view¢er=-33.8569,151.2152&zoom=18&maptype=satellite
--- app/models/map.js
+++ app/models/map.js
@@ -2,6 +2,9 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   latitude: DS.attr('string'),
-  longitude: DS.attr('number')
- 
+  longitude: DS.attr('number'),
+ 	addNum: DS.attr('string'),
+ 	addStreet: DS.attr('string'),
+ 	addCity: DS.attr('string'),
+ 	addState: DS.attr('string')
 });
